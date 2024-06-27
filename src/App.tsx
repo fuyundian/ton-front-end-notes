54e7fbb8ce19a4e36414240de77e4db4a47ceef6
符云典 <>
2024-06-27 14:41:45 +0800

    add twa-sdk

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { TonConnectButton } from "@tonconnect/ui-react";
 import { useMainContract } from "./hooks/useMainContract";
 import { useTonConnect } from "./hooks/useTonConnect";
+import WebApp from '@twa-dev/sdk'
 function App() {
   const {
     contract_address,
@@ -14,6 +15,10 @@ function App() {
     sendWithdrawalRequest
   } = useMainContract();
   const { connected } = useTonConnect()
+
+  const showAlert = () => {
+    WebApp.showAlert("Hey there!");
+  };
   return (
     <div>
       <div>
@@ -21,6 +26,7 @@ function App() {
       </div>
       <div>
         <div className='Card'>
+          <b>{WebApp.platform}</b>
           <b>合约地址</b>
           <div className='Hint'>{contract_address?.slice(0, 30) + "..."}</div>
           <b>合约余额</b>
@@ -34,6 +40,18 @@ function App() {
         <div className='Card'>
           <b>Counter Value</b>
           <div>{counter_value ?? "Loading..."}</div>
+        </div>
+        <div className='Card'>
+          {connected && (
+            <a
+              onClick={() => {
+                showAlert();
+              }}
+            >
+              显示预警
+            </a>
+          )}
+
         </div>
         {connected && (
           <a
